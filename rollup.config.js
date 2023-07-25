cbe6eae127a50f6bb1cfe6bbe6b4438a12f95e67
arjun391c <arjun391c@gmail.com>
2023-07-25 23:49:32 +0530

    final

--- rollup.config.js
+++ rollup.config.js
@@ -1,9 +1,11 @@
 import external from 'rollup-plugin-peer-deps-external'
 import commonjs from '@rollup/plugin-commonjs'
 import resolve from '@rollup/plugin-node-resolve'
-import typescript from '@rollup/plugin-typescript'
+// import typescript from '@rollup/plugin-typescript'
+import typescript from 'rollup-plugin-typescript2';
 import { terser } from 'rollup-plugin-terser'
 import dts from 'rollup-plugin-dts'
+import copy from 'rollup-plugin-copy'
 
 import pkg from './package.json'
 
@@ -12,15 +14,20 @@ export default[
         input: 'src/index.tsx',
         output: [
             {
-                file: pkg.main,
+                dir: "lib/cjs",
                 format: 'cjs',
                 sourcemap: true,
+                preserveModules: true,
+                exports: "named",
                 name: pkg.name
             },
             {
-                file: pkg.module,
+                dir: "lib/esm",
                 format: 'esm',
-                sourcemap: true
+                sourcemap: true,
+                preserveModules: true,
+                exports: "named",
+                name: pkg.name
             }
         ],
         external: [
@@ -28,18 +35,25 @@ export default[
             ...Object.keys(pkg.peerDependencies || {})
         ],
         plugins: [
+            copy({
+                targets: [
+                  { src: ['src/assets/images/icons/*', '!**/*.ts'], dest: 'lib/cjs/assets/images/icons' },
+                  { src: ['src/assets/images/icons/*', '!**/*.ts'], dest: 'lib/esm/assets/images/icons' },
+                ],
+            }),
             external(),
             resolve(),
             commonjs(),
             typescript({ 
-                tsconfig: './tsconfig.json'
+                tsconfig: 'tsconfig.json',
+                useTsconfigDeclarationDir: true
             }),
-            terser()
+            terser(),
         ],
     },
     {
-        input: "dist/esm/types/index.d.ts",
-        output: [{ file: pkg.types, format: "esm" }],
+        input: "lib/types/index.d.ts",
+        output: [{ file: 'lib/index.d.ts', format: "esm" }],
         plugins: [dts()]
     }
 ]
--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,6 +1,7 @@
 import styles from './Button.styles'
-import { Text, TouchableOpacity, TouchableOpacityProps } from 'react-native'
+import { Image, Text, TouchableOpacity, TouchableOpacityProps } from 'react-native'
 import React from 'react'
+import { icn } from '../../assets/images/icons'
 
 export interface ButtonProps extends TouchableOpacityProps {
     label: string,
@@ -14,6 +15,7 @@ const Button = (props: ButtonProps) => {
             {...rest}
         >
             <Text style={styles.text}>{label}</Text>
+            <Image source={icn} style={{height: 20, width: 20}}/>
         </TouchableOpacity>
     )
 }
